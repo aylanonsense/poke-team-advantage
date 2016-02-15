39b48f7b7535b33705a5a5966c414e8e7f111ace
Ayla Myers <ayla@etsy.com>
2016-02-15 15:42:45 -0500

    Can now include pokemon attack types in brackets

--- poke-team-calc.py
+++ poke-team-calc.py
@@ -1,4 +1,5 @@
 import csv
+import re
 
 def parse_csv(filepath, id_field=None):
 	datafile = open(filepath, 'r')
@@ -45,17 +46,28 @@ for offense_type in type_advantages:
 		type_advantages[offense_type][defense_type] = float(type_advantages[offense_type][defense_type])
 
 # read in the user's team of pokemon
-input_string = 'Bulbasaur, Charmander, Squirtle'
+input_string = 'Bulbasaur [Normal Grass Poison], Charmander [Normal Fire], Squirtle [Normal Water]'
 pokemon_names = [x.strip() for x in input_string.split(',')]
 pokemon_team = []
+p = re.compile('(.+) \[(.+)\]')
 for name in pokemon_names:
 	found_pokemon = False
+	match = p.match(name)
+	attack_types = None
+	if match:
+		name = match.group(1)
+		attack_types = set(match.group(2).split(' '))
 	for poke in all_pokemon:
 		if poke['name'] == name:
 			found_pokemon = True
-			pokemon_team.append(poke)
+			pokemon_team.append({
+				'name': poke['name'],
+				'types': poke['types'],
+				'attack_types': (poke['types'] if attack_types is None else attack_types)
+			})
+			break
 	if not found_pokemon:
-		print 'Could not find a Pokemon named %s' % name
+		print 'WARNING! Could not find a Pokemon named %s' % name
 
 # for each type, figure out if this team is checked by it
 doubly_advantageous_matchups = []
@@ -64,11 +76,11 @@ standoff_matchups = []
 disadvantageous_matchups = []
 for opposing_type_combo in all_type_combos:
 	best_matchup = None
-	num_equivalent_matchups = 0
+	best_pokemon_for_matchup = []
 	for poke in pokemon_team:
 		best_offense = None
 		worst_defense = None
-		for team_offense_type in poke['types']:
+		for team_offense_type in poke['attack_types']:
 			multiplier = 1.0
 			for opposing_defense_type in opposing_type_combo:
 				multiplier *= type_advantages[team_offense_type][opposing_defense_type]
@@ -87,38 +99,24 @@ for opposing_type_combo in all_type_combos:
 			matchup = -1
 		if best_matchup is None or matchup > best_matchup:
 			best_matchup = matchup
-			num_equivalent_matchups = 0
+			best_pokemon_for_matchup = []
 		if best_matchup == matchup:
-			num_equivalent_matchups += 1
+			best_pokemon_for_matchup.append(poke['name'])
+	results = { 'type': opposing_type_combo, 'pokemon': best_pokemon_for_matchup }
 	if best_matchup > 0:
-		advantageous_matchups.append(opposing_type_combo)
-		if best_matchup > 0 and num_equivalent_matchups >= 2:
-			doubly_advantageous_matchups.append(opposing_type_combo)
+		advantageous_matchups.append(results)
+		if best_matchup > 0 and len(best_pokemon_for_matchup) >= 2:
+			doubly_advantageous_matchups.append(results)
 	elif best_matchup < 0:
-		disadvantageous_matchups.append(opposing_type_combo)
+		disadvantageous_matchups.append(results)
 	else:
-		standoff_matchups.append(opposing_type_combo)
+		standoff_matchups.append(results)
 
 print 'With a team of %s:' % ', '.join([poke['name'] for poke in pokemon_team])
 print ' There are %i (%i%%) advantageous matchups (%i are covered by more than one pokemon)' % (len(advantageous_matchups), 100 * len(advantageous_matchups) / len(all_type_combos), len(doubly_advantageous_matchups))
 print ' There are %i (%i%%) standoffs:' % (len(standoff_matchups), 100 * len(standoff_matchups) / len(all_type_combos))
 for matchup in standoff_matchups:
-	print '  vs. %s' % " ".join(matchup)
+	print '  vs. %s (best matchup: %s)' % (" ".join(matchup['type']).ljust(25), ", ".join(matchup['pokemon']))
 print ' There are %i (%i%%) disadvantageous matchups:' %(len(disadvantageous_matchups), 100 * len(disadvantageous_matchups) / len(all_type_combos))
 for matchup in disadvantageous_matchups:
-	print '  vs. %s' % " ".join(matchup)
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+	print '  vs. %s (best matchup: %s)' % (" ".join(matchup['type']).ljust(25), ", ".join(matchup['pokemon']))
